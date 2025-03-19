7715a833b20fdd4ca16ab8c599347182c5bff9da
alexherranzalba <kyeownz@gmail.com>
2025-03-19 15:40:05 +0100

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,13 @@ import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
